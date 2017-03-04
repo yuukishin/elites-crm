96184b1a3c5668134372def5b39daac3dfc55ec8
Yuki Shin <silversoul1024@gmail.com>
2017-03-04 05:05:10 +0000

    05実習完了

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,11 @@
 class CustomersController < ApplicationController
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  
   def index
     # @customers = Customer.all
-    @customers = Customer.page(params[:page])
+    # @customers = Customer.page(params[:page])
+    @q = Customer.search(params[:q])
+    @customers = @q.result(distinct: true).page(params[:page])
   end
 
   def new
